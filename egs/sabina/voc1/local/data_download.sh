0a3b61fa0c6e0a98ec7e62bf97a58cfb91f04877
Airenas Vaičiūnas <airenass@gmail.com>
2020-10-19 11:13:31 +0300

    Add sabina eg

---
+++ egs/sabina/voc1/local/data_download.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Copyright 2019 Tomoki Hayashi
+#  MIT License (https://opensource.org/licenses/MIT)
+
+download_dir=$1
+
+# check arguments
+if [ $# != 1 ]; then
+    echo "Usage: $0 <download_dir>"
+    exit 1
+fi
+
+set -euo pipefail
+
+cwd=$(pwd)
+if [ ! -e "${download_dir}/sabina.v02" ]; then
+    mkdir -p "${download_dir}"
+    cd "${download_dir}"
+    tar -vxf ./*.tar.gz
+    rm ./*.tar.gz
+    cd "${cwd}"
+    echo "successfully prepared data."
+else
+    echo "already exists. skipped."
+fi
