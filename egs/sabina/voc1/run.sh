c02c513955972ad9730334a8ee732a6394bd1d42
Airenas Vaičiūnas <airenass@gmail.com>
2022-03-22 09:50:37 +0200

    Update sabina training scripts
    copied from laimis

--- egs/sabina/voc1/run.sh
+++ egs/sabina/voc1/run.sh
@@ -11,14 +11,15 @@ stage=-1       # stage to start
 stop_stage=100 # stage to stop
 verbose=1      # verbosity level (lower is less info)
 n_gpus=1       # number of gpus in training
-n_jobs=16      # number of parallel jobs in feature extraction
+n_jobs=${n_jobs}      # number of parallel jobs in feature extraction
+m_jobs=${m_jobs}
 
 # NOTE(kan-bayashi): renamed to conf to avoid conflict in parse_options.sh
-conf=conf/parallel_wavegan.v3.yaml
+conf=${train_config}
 
 # directory path setting
 download_dir=downloads # direcotry to save downloaded files
-dumpdir=dump           # directory to dump features
+dumpdir=${dump_dir}           # directory to dump features
 
 # training related setting
 tag=""     # tag for directory to save model
@@ -39,6 +40,8 @@ eval_set="eval"         # name of evaluation data direcotry
 
 set -euo pipefail
 
+echo "Train config: ${conf}"
+
 if [ "${stage}" -le -1 ] && [ "${stop_stage}" -ge -1 ]; then
     echo "Stage -1: Data download"
     local/data_download.sh "${download_dir}"
@@ -50,7 +53,7 @@ if [ "${stage}" -le 0 ] && [ "${stop_stage}" -ge 0 ]; then
         --train_set "${train_set}" \
         --dev_set "${dev_set}" \
         --eval_set "${eval_set}" \
-        "${download_dir}/sabina.v02" data
+        "${download_dir}/${corpus}" data
 fi
 
 stats_ext=$(grep -q "hdf5" <(yq ".format" "${conf}") && echo "h5" || echo "npy")
@@ -63,12 +66,9 @@ if [ "${stage}" -le 1 ] && [ "${stop_stage}" -ge 1 ]; then
         [ ! -e "${dumpdir}/${name}/raw" ] && mkdir -p "${dumpdir}/${name}/raw"
         echo "Feature extraction start. See the progress via ${dumpdir}/${name}/raw/preprocessing.*.log."
         utils/make_subset_data.sh "data/${name}" "${n_jobs}" "${dumpdir}/${name}/raw"
-        ${train_cmd} JOB=1:${n_jobs} "${dumpdir}/${name}/raw/preprocessing.JOB.log" \
-            parallel-wavegan-preprocess \
-                --config "${conf}" \
-                --scp "${dumpdir}/${name}/raw/wav.JOB.scp" \
-                --dumpdir "${dumpdir}/${name}/raw/dump.JOB" \
-                --verbose "${verbose}"
+        tmp_s=""
+        for i in `eval echo {1..${n_jobs}}`; do tmp_s="${tmp_s} preprocess-${i}"; done
+        make ${tmp_s} dump_dir="${dumpdir}/${name}/raw" verbose="${verbose}" -j ${m_jobs}
         echo "Successfully finished feature extraction of ${name} set."
     ) &
     pids+=($!)
@@ -93,13 +93,11 @@ if [ "${stage}" -le 1 ] && [ "${stop_stage}" -ge 1 ]; then
     (
         [ ! -e "${dumpdir}/${name}/norm" ] && mkdir -p "${dumpdir}/${name}/norm"
         echo "Nomalization start. See the progress via ${dumpdir}/${name}/norm/normalize.*.log."
-        ${train_cmd} JOB=1:${n_jobs} "${dumpdir}/${name}/norm/normalize.JOB.log" \
-            parallel-wavegan-normalize \
-                --config "${conf}" \
-                --stats "${dumpdir}/${train_set}/stats.${stats_ext}" \
-                --rootdir "${dumpdir}/${name}/raw/dump.JOB" \
-                --dumpdir "${dumpdir}/${name}/norm/dump.JOB" \
-                --verbose "${verbose}"
+        tmp_s=""
+        for i in `eval echo {1..${n_jobs}}`; do tmp_s="${tmp_s} normalize-${i}"; done
+        # ${train_cmd} JOB=1:${n_jobs} "${dumpdir}/${name}/norm/normalize.JOB.log" \
+        make ${tmp_s} norm_dir="${dumpdir}/${name}" verbose="${verbose}" stats="${dumpdir}/${train_set}/stats.${stats_ext}" \
+            -j ${m_jobs}
         echo "Successfully finished normalization of ${name} set."
     ) &
     pids+=($!)
